82ce57ea1ddf9fb7ca3d7c90cea967da6c3d7e1a
Guilherme Vieira <super.driver.512@gmail.com>
2017-11-29 01:41:40 -0200

    Improve example a bit.

--- example.js
+++ example.js
@@ -1,7 +1,9 @@
 let sh = require('blastoise-shell');
 
 async function main() {
+  await sh.echo('Hello, world.').sed('s/world/my friend/');
   await sh.cat('example.js').grep('example');
+  await sh('notify-send', `i hackz ur computerz`);
 }
 
 main().catch(console.error);
