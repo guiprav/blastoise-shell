d3f07863b371e1d901f4989f23747f63c36318d0
Guilherme Vieira <super.driver.512@gmail.com>
2017-11-29 03:08:54 -0200

    Update examples.

--- example.js
+++ example.js
@@ -1,9 +1,22 @@
 let sh = require('blastoise-shell');
 
 async function main() {
+  // echo "Hello, world." | sed "s/world/my friend/"
   await sh.echo('Hello, world.').sed('s/world/my friend/');
+
+  // echo "Hello, world." >> hellos
+  await sh.echo('Hello, world.').appendTo('hellos');
+
+  // cat hellos
+  console.log(await sh.cat('hellos').toString());
+
+  // cat example.js | grep example
   await sh.cat('example.js').grep('example');
+
+  // notify-send "i hackz ur computerz"
   await sh('notify-send', `i hackz ur computerz`);
+
+  process.exit();
 }
 
 main().catch(console.error);
